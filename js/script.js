e0094e0a410fa305901eef385feaec6548a556a0
taomao <kennymao@outlook.com>
2021-04-28 16:18:02 -0500

    4.27

--- js/script.js
+++ js/script.js
@@ -1,15 +1,58 @@
 let ctx = document.getElementById('myChart').getContext('2d');
-let labels = ['Tencent','I Spent on Tencent', 'Nintendo','I Spent on Nintendo', 'Sony','I Spent on Sony', 'Apple','I Spent on Apple','Google','I Spent on Google','Microsoft','I Spent on Microsoft',];
-let colorHex = ['#FB3640', '#ED9236', '#43AA8B', '#2BB673','#EAD939','#FCBC08','#087DFC','#064589','#9543FF','#E280F9','#FF0B0B','#F94C1E'];
+let labels = ['Tencent', 'Nintendo', 'Sony', 'Apple','Google','Microsoft'];
+let colorHex = ['#FB3640','#43AA8B','#EAD939','#FF0B0B','#087DFC','#9543FF'];
 
 let myChart = new Chart(ctx, {
   type: 'pie',
   data: {
     datasets: [{
-      data: [54594000000,120,10914000000,1013,76929000000,1420,260174000000,2164,160740000000,420,70697000000,3720],
+      data: [54594000000,10914000000,76929000000,260174000000,160740000000,70697000000],
       backgroundColor: colorHex
     }],
     labels: labels
+  },
+  options: {
+    responsive: true,
+    legend: {
+      position: 'bottom'
+    },
+    plugins: {
+      datalabels: {
+        color: '#fff',
+        anchor: 'end',
+        align: 'start',
+        offset: -10,
+        borderWidth: 2,
+        borderColor: '#fff',
+        borderRadius: 25,
+        backgroundColor: (context) => {
+          return context.dataset.backgroundColor;
+        },
+        font: {
+          weight: 'bold',
+          size: '10'
+        },
+        formatter: (value) => {
+          return value + ' %';
+        }
+      }
+    }
+  }
+})
+
+
+let ctx2 = document.getElementById('myChart2').getContext('2d');
+let labels2 = ['I Spent on Tencent', 'I Spent on Nintendo','I Spent on Sony','I Spent on Apple','I Spent on Google','I Spent on Microsoft',];
+let colorHex2 = [ '#FB3640','#43AA8B','#EAD939','#FF0B0B','#087DFC','#9543FF'];
+
+let myChart2 = new Chart(ctx2, {
+  type: 'pie',
+  data: {
+    datasets: [{
+      data: [120,1013,1420,2164,420,3720],
+      backgroundColor: colorHex2
+    }],
+    labels: labels2
   },
   options: {
     responsive: true,
